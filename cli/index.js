8a7c124a43fc8b307afae5298ca9b3549cf14a95
Jake Hamilton <jakehamtexas@gmail.com>
2021-04-30 14:35:18 -0500

    Add debug logging and debug optional argument to main command.

--- cli/index.js
+++ cli/index.js
@@ -4,6 +4,9 @@ const main = require('./main');
 
 const cli = new Command();
 
-cli.arguments("[dir] [url]").action(main);
+cli
+  .arguments("[dir] [url]")
+  .option("-d, --debug", "Enable debug logging.")
+  .action(main);
 
 module.exports = cli;
--- cli/main.js
+++ cli/main.js
@@ -1,14 +1,18 @@
-const { getBody, getTracks, validateTracks, downloadTrack } = require("../lib");
+const { getBody, getTracks, validateTracks, downloadTrack, util: { debugLogBy } } = require("../lib");
 
 const defaults = {
   DIR: "/Users/pat/Documents/Audiobooks",
   URL: "https://tokybook.com/tales-from-earthsea/",
 };
 
-const main = async (dir = defaults.DIR, url = defaults.URL) => {
+const main = async (dir = defaults.DIR, url = defaults.URL, { debug: isDebugEnabled }) => {
+  const debugLog = debugLogBy(isDebugEnabled);
+  debugLog(dir, url);
+
   const body = await getBody(url);
   const unvalidatedTracks = getTracks(body);
   const tracks = validateTracks(unvalidatedTracks);
+  
   await Promise.all(tracks.map((track) => downloadTrack(track, dir)));
 };
 
