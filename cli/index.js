d72c96ebb7eed4e1ff98a84fc94268ba4469a87e
Patrick Tone <patrick@madronesoftware.com>
2021-05-20 15:00:30 +0200

    Adds ESLint (#19)
    * Adds eslint with airbnb rules
    
    * Adds pre-commit hook to run tests before committing

--- cli/index.js
+++ cli/index.js
@@ -5,8 +5,8 @@ const main = require('./main');
 const cli = new Command();
 
 cli
-  .arguments("[dir] [url]")
-  .option("-d, --debug", "Enable debug logging.")
+  .arguments('[dir] [url]')
+  .option('-d, --debug', 'Enable debug logging.')
   .action(main);
 
 module.exports = cli;
