172e5f6a5e22918ff76d7737e070ea555dd520a7
Jake Hamilton <jakehamtexas@gmail.com>
2021-05-24 13:34:41 -0500

    Convert lib/getBody.js to typescript

--- src/lib/getBody.ts
+++ src/lib/getBody.ts
@@ -1,6 +1,6 @@
 import got from 'got';
 
-const getBody = async (url) => {
+const getBody = async (url: string) => {
   const response = await got(url);
   const { body } = response;
   return body;
