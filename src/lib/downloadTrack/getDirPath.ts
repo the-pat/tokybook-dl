66116732970ce75f8c7926e94496a4ca37bcfd31
Jake Hamilton <jakehamtexas@gmail.com>
2021-05-24 15:31:07 -0500

    Add case package and make test pass

--- src/lib/downloadTrack/getDirPath.ts
+++ src/lib/downloadTrack/getDirPath.ts
@@ -1,11 +1,20 @@
 import path from 'path';
+import Case from 'case';
 import { TrackWithUrl } from 'types/lib';
 
 const getDirPath = (track: TrackWithUrl, dir: string) => {
-  const [, , filename] = track.chapter_link_dropbox.split('/');
-  const fileparts = filename.split(' - ');
-  const book = fileparts[0];
-  return path.join(dir, book);
+  const dropboxLinkComponents = track.chapter_link_dropbox.split('/');
+
+  let [bookTitleFromDropboxURL] = dropboxLinkComponents;
+  bookTitleFromDropboxURL = Case.capital(bookTitleFromDropboxURL);
+
+  const [, , filename] = dropboxLinkComponents;
+  const [bookTitleFromFilename] = filename.split(' - ');
+
+  const bookTitle = bookTitleFromFilename === filename
+    ? bookTitleFromDropboxURL
+    : bookTitleFromFilename;
+  return path.join(dir, bookTitle);
 };
 
 export default getDirPath;
