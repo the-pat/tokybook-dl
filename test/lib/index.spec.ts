a6231f20a9da7099910aedd2291dab16b9c8d98d
Jake Hamilton <jakehamtexas@gmail.com>
2021-05-24 15:33:09 -0500

    Add test for preferring book name in final URL component.

--- test/lib/index.spec.ts
+++ test/lib/index.spec.ts
@@ -2,6 +2,19 @@ import getDirPath from '../../src/lib/downloadTrack/getDirPath';
 import { TrackWithUrl } from 'types/lib';
 
 describe('getDirPath', () => {
+  it('should prefer the book name given in the final URL component', () => {
+    // arrange
+    const track = { chapter_link_dropbox: "probably-garbage/12345/A Book - a-file.mp3" } as TrackWithUrl;
+    const dir = "path/to/books";
+    const expectedDirPath = "path/to/books/A Book";
+
+    // act
+    const actualDirPath = getDirPath(track, dir);
+
+    // assert
+    expect(actualDirPath).toEqual(expectedDirPath);
+  });
+
   it('should give a dir path regardless of whether the book name is in the final URL component', () => {
     // arrange
     const track = { chapter_link_dropbox: "a-book/12345/a-file.mp3" } as TrackWithUrl;
