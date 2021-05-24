0fdab87e6a5e0fbb3f5e4dc6ff3b6dd686688ae6
Jake Hamilton <jakehamtexas@gmail.com>
2021-05-24 15:16:58 -0500

    Create failing test for getDirPath

--- test/lib/index.spec.ts
+++ test/lib/index.spec.ts
@@ -1,5 +1,17 @@
+import getDirPath from '../../src/lib/downloadTrack/getDirPath';
+import { TrackWithUrl } from 'types/lib';
+
 describe('getDirPath', () => {
   it('should give a dir path regardless of whether the book name is in the final URL component', () => {
-    expect(2 + 2).toEqual(4);
+    // arrange
+    const track = { chapter_link_dropbox: "a-book/12345/a-file.mp3" } as TrackWithUrl;
+    const dir = "path/to/books";
+    const expectedDirPath = "path/to/books/A Book";
+
+    // act
+    const actualDirPath = getDirPath(track, dir);
+
+    // assert
+    expect(actualDirPath).toEqual(expectedDirPath);
   });
 });
