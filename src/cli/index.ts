e101c2a403735ec880c883607bc26c2df82be841
Jake Hamilton <jakehamtexas@gmail.com>
2021-05-30 23:37:23 -0500

    Add description for main command

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -9,6 +9,10 @@ const cli = new Command();
 
 cli
   .arguments('[dir] [url]')
+  .description('Download an audio book into the supplied directory, using the book located at the supplied url.', {
+    dir: 'Parent directory to save audio books to. Audio book chapters will be downloaded into a directory named by the title of the book.',
+    url: 'The target https://tokybook.com/:book-name URL.',
+  })
   .option('-d, --debug', 'Enable debug logging.')
   .option(
     '-l, --limit <number>',
