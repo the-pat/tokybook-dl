f9a6dab883579d720db88dc746d51f3299ae2ede
Jake Hamilton <jakehamtexas@gmail.com>
2021-05-30 22:36:09 -0500

    Move defaults for dir and url to defaults enum.

--- src/cli/main.ts
+++ src/cli/main.ts
@@ -1,16 +1,13 @@
 import { MainCommandOptions } from 'types/cli';
+import settings from 'const/settings';
+
 import {
   getBody, getTracks, validateTracks, logger, downloadTracks,
 } from '../lib';
 
-const defaults = {
-  DIR: '.',
-  URL: 'https://tokybook.com/tales-from-earthsea/',
-};
-
 const main = async (
-  dir = defaults.DIR,
-  url = defaults.URL,
+  dir = settings.defaults.directory,
+  url = settings.defaults.url,
   { debug: isDebugEnabled, limit: concurrency }: MainCommandOptions,
 ) => {
   logger.level = isDebugEnabled ? 'debug' : 'info';
--- src/const/settings.ts
+++ src/const/settings.ts
@@ -1,6 +1,8 @@
 const settings = {
   defaults: {
     concurrency: Number.MAX_SAFE_INTEGER,
+    directory: '.',
+    url: 'https://tokybook.com/tales-from-earthsea/',
   },
 } as const;
 
