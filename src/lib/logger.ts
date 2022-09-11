d9a5f833ee5c1cc220275cb50357dff0cdc1f598
Jake Hamilton <jakehamtexas@gmail.com>
2022-09-11 12:54:24 -0500

    log errors to stderr instead of stdout

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,7 +1,21 @@
 import pino from "pino";
 
-const logger = pino({
+const stdoutLogger = pino({
   prettyPrint: true,
 });
 
-export default logger;
+const STDERR_STREAM = 2 as const;
+const stderrLogger = pino(
+  {
+    prettyPrint: true,
+  },
+  pino.destination(STDERR_STREAM)
+);
+
+export type Logger = Pick<pino.Logger, "debug" | "info" | "error"> & {
+  level?: "fatal" | "error" | "warn" | "info" | "debug" | "trace";
+};
+
+stdoutLogger.error = stderrLogger.error.bind(stderrLogger);
+
+export default stdoutLogger as Logger;
