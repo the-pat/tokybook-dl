99c22e7fce8a60b9393e171f3ac065498e8ffc53
Jake Hamilton <jakehamtexas@gmail.com>
2022-09-11 12:51:39 -0500

    simplify entrypoint and add shebang

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-import { logger } from './lib';
-import cli from './cli';
+#!/usr/bin/env node
 
-(async () => {
-  try {
-    await cli.parseAsync();
-  } catch (error) {
-    logger.error(error);
-    process.exit(1);
-  }
-})();
+import { logger } from "./lib";
+import cli from "./cli";
+
+cli.parseAsync().catch((error) => {
+  logger.error(error);
+  process.exit(1);
+});
